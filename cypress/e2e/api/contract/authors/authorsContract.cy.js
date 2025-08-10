8d5a4f1e03e712f464818fa2af079619361bd7db
Leandro Tadeu Alves <leandro.alves@dbserver.com.br>
2025-08-10 20:22:53 -0300

    adicionado variável mockResponse para tratativa de erros de forma mockada

--- cypress/e2e/api/contract/authors/authorsContract.cy.js
+++ cypress/e2e/api/contract/authors/authorsContract.cy.js
@@ -1,12 +1,19 @@
 import getAuthorsById from '../../../../schemas/authors/getAuthorsById.json'
 
 describe('Contrato no módulo Authors', () => {
-    context('Validando contratos de sucesso', function () {
-        it('deve validar status e schema', () => {
-            cy.request('/api/v1/Authors').then((response) => {
-                expect(response.status).to.eq(200)
-                cy.validateSchema(getAuthorsById, response.body)
-            })
-        })
+  context('Validando contratos de sucesso', function () {
+    it('deve validar status e schema', () => {
+      const mockResponse = [
+        {
+          id: true,
+          idBook: 'Foto de capa',
+          url: 'https://exemplo.com/autor.jpg'
+        }
+      ]
+      cy.request('/api/v1/Authors').then((response) => {
+        expect(response.status).to.eq(200)
+        cy.validateSchema(getAuthorsById, mockResponse)
+      })
     })
+  })
 })
